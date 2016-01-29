bee00f94ae0ae3becb33a84e519d8e4cd6e5d2c7
astrojhgu <astrojhgu@gmail.com>
2016-01-29 18:03:35 +0800

    修改：     include/adapt_trapezoid.hpp

--- include/adapt_trapezoid.hpp
+++ include/adapt_trapezoid.hpp
@@ -11,44 +11,38 @@
 
 //using namespace std;
 
-template<typename T1,typename T2,typename T3>
-class triple
+template<typename T>
+class interval_sum
 {
  public:
-  T1 first;
-  T2 second;
-  T3 third;
-  triple(T1 x1,T2 x2,T3 x3)
-    :first(x1),second(x2),third(x3)
+  T left_x;
+  T ydx;
+  bool to_be_refined;
+  T err;
+  interval_sum(T x1,T x2,bool x3)
+    :left_x(x1),ydx(x2),to_be_refined(x3)
     {
     }
 };
 
 
-template<typename T1,typename T2,typename T3>
-triple<T1,T2,T3> make_triple(T1 x1,T2 x2,T3 x3)
+template <typename fT,typename T>
+T adapt_trapezoid(fT fun,
+		  T x1,
+		  T x2,
+		  T err_limit,
+		  size_t max_intervals=0)
 {
-  return triple<T1,T2,T3>(x1,x2,x3);
-}
-
-
-template <typename fT,typename T1,typename T2>
-auto adapt_trapezoid(fT fun,
-		     T1 x1,
-		     T2 x2,
-		     decltype(x1+x2) err_limit) -> decltype(fun((x1+x2)))
-{
-  typedef decltype(x1+x2) T;
-  typedef triple<T,T,bool> interval;
-  typedef decltype(fun(x1)) resT;
+  typedef interval_sum<T> interval;
+  
   /*T for interval type,
     bool for state trur for still to be updated,
     false for do not need to be updated
    */
   std::list<interval> interval_list;
-  resT current_sum=((fun(x1)+fun(x2))/(T)(2)*(x2-x1));
-  interval_list.push_back(make_triple(x1,current_sum,true));
-  interval_list.push_back(make_triple(x2,(T)0.,true));
+  T current_sum=((fun(x1)+fun(x2))/(T)(2)*(x2-x1));
+  interval_list.push_back(interval(x1,current_sum,true));
+  interval_list.push_back(interval(x2,(T)0.,true));
   bool int_state=1;
   size_t n_intervals=1;
   while(int_state)
@@ -60,29 +54,29 @@ auto adapt_trapezoid(fT fun,
       i2++;
       for(;i2!=interval_list.end();i1++,i2=i1,i2++)
 	{
-	  //cout<<i1->first<<"\t"<<i2->first<<endl;
-	  //assert(i2->first>i1->first);
-	  if(i1->third)
+	  //cout<<i1->left_x<<"\t"<<i2->left_x<<endl;
+	  //assert(i2->left_x>i1->left_x);
+	  if(i1->to_be_refined)
 	    {
-	      interval new_interval((i1->first+i2->first)/2,0,true);
+	      interval new_interval((i1->left_x+i2->left_x)/2,0,true);
 	      
-	      resT sum1,sum2;
-	      sum1=(fun(new_interval.first)+fun(i1->first))/2*(new_interval.first-i1->first);
-	      sum2=(fun(new_interval.first)+fun(i2->first))/2*(i2->first-new_interval.first);
-	      new_interval.second=sum2;
-	      resT err;
-	      err=i1->second-sum1-sum2;
+	      T sum1,sum2;
+	      sum1=(fun(new_interval.left_x)+fun(i1->left_x))/2*(new_interval.left_x-i1->left_x);
+	      sum2=(fun(new_interval.left_x)+fun(i2->left_x))/2*(i2->left_x-new_interval.left_x);
+	      new_interval.ydx=sum2;
+	      T err;
+	      err=i1->ydx-sum1-sum2;
 	      err/=(sum1+sum2);
 	      
 	      err=err<0?-err:err;
 
 	      if(err>err_limit||n_intervals<10)
 		{
-		  i1->second=sum1;
+		  i1->ydx=sum1;
 		  interval_list.insert(i2,new_interval);
 		  n_intervals++;
 		  //cout<<n_intervals<<'\n';;
-		  if(n_intervals>10000)
+		  if(max_intervals>0&&n_intervals>max_intervals)
 		    {
 		      std::cerr<<"Warning: Division of the integral interval exceeds 10000, we shall stop here, for the consumption of CPU time. Be care of the result."<<std::endl;
 		      int_state=0;
@@ -92,7 +86,7 @@ auto adapt_trapezoid(fT fun,
 		}
 	      else
 		{
-		  i1->third=false;
+		  i1->to_be_refined=false;
 		}
 	      int_state=1;
 	    }
@@ -100,15 +94,15 @@ auto adapt_trapezoid(fT fun,
       
     }
  bk:
-  resT result=0;
+  T result=0;
   
   interval_list.sort([](const interval& x1,const interval& x2){
-      return std::abs(x1.second)<std::abs(x2.second);
+      return std::abs(x1.ydx)<std::abs(x2.ydx);
     }
     );
 
-  result=std::accumulate(interval_list.begin(),interval_list.end(),interval(0,0,false),[](const interval& x1,const interval& x2){return interval(0,x1.second+x2.second,false);}).second;
-  
+  result=std::accumulate(interval_list.begin(),interval_list.end(),interval(0,0,false),[](const interval& x1,const interval& x2){return interval(0,x1.ydx+x2.ydx,false);}).ydx;
+  //std::cout<<interval_list.size()<<std::endl;
   return result;
 }
 
